8ad67ae87e8ea5b9bc020937b9fc644160321444
buy-st <stalatala@byupathway.edu>
2025-05-12 23:32:45 +0200

    final commit on  few files

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/my_portofolio/',  // Replace with your GitHub repo name
+  build: {
+    outDir: 'build', // Optional: ensures the build folder is called "build"
+  }
 })
+
